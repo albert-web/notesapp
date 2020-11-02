5343e5a43ae7976281557de90cce2d3b4b6b63b7
albertmensah <DMensah@madisoncollege.edu>
2020-11-02 13:39:55 -0600

    attemptig to publish

---
+++ docs/precache-manifest.d68b10edbcb4948a20f7a7751bf7236a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a278933c3434b4b1f28e75d60177fd3",
+    "url": "/notesapp/index.html"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/notesapp/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "83e73e4d3344a4b70fbf",
+    "url": "/notesapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/notesapp/static/js/2.13bf3f25.chunk.js"
+  },
+  {
+    "revision": "7c68b657c7cc254f4ca9b56af0cb6fab",
+    "url": "/notesapp/static/js/2.13bf3f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e73e4d3344a4b70fbf",
+    "url": "/notesapp/static/js/main.a08d1891.chunk.js"
+  },
+  {
+    "revision": "62ccf5cbe480d591582e",
+    "url": "/notesapp/static/js/runtime-main.d30671ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notesapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
